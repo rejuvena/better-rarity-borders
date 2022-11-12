7713d716fbc8d8426ad684ba3a7c2e9615c14e55
Tomat <xxlennygamerxx@gmail.com>
2022-11-11 17:27:54 -0800

    Ugly unfinished borders.

--- Rendering/Borders/BorderRenderingSystem.cs
+++ Rendering/Borders/BorderRenderingSystem.cs
@@ -1,15 +1,31 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.Enums;
+using Terraria.GameContent.UI;
+using Terraria.ModLoader;
 
 namespace BetterRarityBorders.Rendering.Borders;
 
 public sealed class BorderRenderingSystem : IRenderingSystem
 {
-    void IRenderingSystem.Update(bool inItemSlot, ItemDrawData itemDrawData) {
-    }
+    void IRenderingSystem.Update(bool inItemSlot, ItemDrawData itemDrawData) { }
 
     void IRenderingSystem.DrawBefore(SpriteBatch sb, bool inItemSlot, ItemDrawData itemDrawData) {
+        if (!inItemSlot) return;
+        var texture = ModContent.Request<Texture2D>("BetterRarityBorders/Assets/Default").Value;
+        sb.Draw(
+            texture,
+            itemDrawData.ItemPosition - itemDrawData.ItemOrigin,
+            null,
+            ItemRarity.GetColor(itemDrawData.Item.rare),
+            0f,
+            texture.Size() / 2f,
+            Main.inventoryScale,
+            SpriteEffects.None,
+            0f
+        );
     }
 
-    void IRenderingSystem.DrawAfter(SpriteBatch sb, bool inItemSlot, ItemDrawData itemDrawData) {
-    }
+    void IRenderingSystem.DrawAfter(SpriteBatch sb, bool inItemSlot, ItemDrawData itemDrawData) { }
 }
